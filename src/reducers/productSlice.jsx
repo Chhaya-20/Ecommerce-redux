39f7e3e12f4b25a21c639f239c421111e3dcf240
Chhaya-20 <157710352+Chhaya-20@users.noreply.github.com>
2024-03-28 14:56:00 +0530

    productSlice.jsx

--- src/reducers/productSlice.jsx
+++ src/reducers/productSlice.jsx
@@ -7,15 +7,16 @@ const todoSlice = createSlice({
   },
   reducers: {
     addProduct: (state, action) => {
-      const { id, name, price, image , description , brand } = action.payload;
+      const { id, name, price, image , description , brand} = action.payload;
       const existingIndex = state.todos.findIndex(item => item.id === id);
     
       if (existingIndex === -1) {
-        state.todos.push({ id, name, price, quantity: 1, image, description , brand });
+        state.todos.push({ id, name, price, quantity: 1, image, description , brand,totalprice :price });
       } else {
         state.todos[existingIndex] = {
           ...state.todos[existingIndex],
-          quantity: state.todos[existingIndex].quantity + 1
+          quantity: state.todos[existingIndex].quantity + 1,
+          totalprice:state.todos[existingIndex].totalprice+ state.todos[existingIndex].price
         };
       }
     },
@@ -23,20 +24,19 @@ const todoSlice = createSlice({
     increase: (state, action) => {
       const existingIndex = state.todos.findIndex(item => item.id === action.payload);
       state.todos[existingIndex].quantity++;
-      state.todos[existingIndex].price=state.todos[existingIndex].price+state.todos[existingIndex].price;
+      state.todos[existingIndex].totalprice=state.todos[existingIndex].totalprice+state.todos[existingIndex].price;
 
     },
     decrease: (state, action) => {
       const existingIndex = state.todos.find(item => item.id === action.payload);
       if(existingIndex.quantity==1)
       {
-        ////console.log("jek");
         state.todos = state.todos.filter((todo) => todo.id !== action.payload);
       }
       else{
         const Index = state.todos.findIndex(item => item.id === action.payload);
         state.todos[Index].quantity--;
-       state.todos[Index].price=state.todos[Index].price-state.todos[Index].price;
+        state.todos[Index].totalprice=state.todos[Index].totalprice-state.todos[Index].price;
       }
       
     },
